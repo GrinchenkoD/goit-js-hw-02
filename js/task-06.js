0e6804fab31ab19a7e50fe23daabb68bc7cb94c7
Dmitry Grinchenko <ra1d3r@i.ua>
2020-12-17 16:56:40 +0200

    юху! победил task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,30 @@
 // 2)Ввод сохраняется в переменную input и добавляется в массив чисел numbers
 // 3) ввод продолжается до тех пор, пока пользователь не нажмет Cancel.
 // 4) После того как пользователь прекратил ввод нажав Cancel:
-//     - проверить пустой ли массив ил нет;
 //     -посчитать сумму всех элементов массива и записать ее в переменную total
 //     -в консоль выведи строку 'Общая сумма чисел равна [сумма]'
-
 
 let input;
 const numbers = [];
 let total = 0;
+
+do {
+  input = prompt('Введите число');
+
+  if (input === null) {
+    for (const number of numbers) {
+      total += number;
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+    break;
+  }
+  input = Number(input);
+
+  const notANumber = Number.isNaN(input);
+
+  if (notANumber) {
+    alert('Было введено не число, попробуйте еще раз');
+    continue;
+  }
+  numbers.push(input);
+} while (input !== null);
