4e3ce29700354604a2e0ac5ad584e1e399f00c8b
Dmitry Grinchenko <ra1d3r@i.ua>
2020-12-17 10:03:32 +0200

    rdy 1-2-3-4-5

--- js/task-04.js
+++ js/task-04.js
@@ -5,22 +5,23 @@
 // если больше 40 символов вернет укороченную (обрезать по 40й символ и добавить '...')
 
 
-const formatString = function(string) {
-    let symbols = string.split('');
-    let optimizedSymbols;
-    let optimizedString;
-    console.log(string.legnth);
-    if (symbols.legnth > 40) {
-         symbols.splice(0, 40);
-        console.log(symbols);
+const formatString = function (string) {
+    let newString;
+    const symbols = string.split('');
+    if (symbols.length > 40) {
+        const newSymbols = symbols.slice(0, 40);
+        newString = newSymbols.join('');
+        return `${newString}...`;   
     }
-
+    else
+        return newString = string;
+     
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // вернется оригинальная строка
 
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
---
+++ js/task-05.js
@@ -0,0 +1,33 @@
+'use strict';
+//     Слова в строке могут быть в произвольном регистре.
+// Функция проверяет ее на содержание слов spam и sale
+// Если нашли зарещенное слово то функция возвращает true,
+//     если запрещенных слов нет функция возвращает false
+
+
+
+
+
+const checkForSpam = function (message) {
+    message = message.toLowerCase();
+
+    if (message.includes('spam') || message.includes('sale')) {
+        return true;
+        
+    } else {
+        return false;
+    }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
